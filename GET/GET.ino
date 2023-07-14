7f48a1a8b2e8a80549996d4770da689b2ee4d288
Casey Boswell <caseyjane2013@gmail.com>
2023-07-14 12:14:21 +1000

    Tristan Pushing Working Code

--- GET/GET.ino
+++ GET/GET.ino
@@ -18,6 +18,7 @@
 #include "SR04.h"
 
 #define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define POT_PIN A0
 
 // ON/OFF Switch
 const int buttonPin = 9;    // the number of the pushbutton pin
@@ -28,10 +29,13 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 // Chest Ultrasonic Sensor
-#define ULTRASONIC_TRIG_PIN 26
-#define ULTRASONIC_ECHO_PIN 25
+#define ULTRASONIC_TRIG_PIN 13
+#define ULTRASONIC_ECHO_PIN 14
 SR04 ultrasonic_sensor = SR04(ULTRASONIC_ECHO_PIN, ULTRASONIC_TRIG_PIN);
 long chest_distance;
+
+// Leg Potentiometer
+int leg_angle;
 
 // Controls
 int PUSH_UP_DISTANCE = 20; // cm
@@ -39,12 +43,13 @@ int SQUAT_KNEE_ANGLE = 90; // degrees
 
 // Set these to your desired credentials.
 const char *ssid = "GET";
-//const char *password = "good";
+const char *password = "good";
 
 WiFiServer server(80);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
 
   Serial.begin(115200);
   Serial.println();
@@ -61,23 +66,37 @@ void setup() {
 }
 
 void loop() {
+    
 
   // Collect & record chest distance measure
   chest_distance = ultrasonic_sensor.Distance();
+  Serial.println(chest_distance);
   if (chest_distance <  PUSH_UP_DISTANCE) {
     Serial.println("Pushup detected.");
 //      client.print("<script>pushup=true</script>");
   }
   printSensorValues();
+
+// Collect & record leg angle
+  int pot_value = analogRead(POT_PIN);
+  leg_angle = map(pot_value, 0, 4096, 0, 300);
+  if (leg_angle < 150) {
+    //Serial.println("S Q U A T!!!!");
+  }
+
+
   
   WiFiClient client = server.available();   // listen for incoming clients
   if (client) {                             // if you get a client,
     Serial.println("New Client.");           // print a message out the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
+    
     while (client.connected()) {            // loop while the client's connected
+      
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
+        
         if (c == '\n') {                    // if the byte is a newline character
 
           // if the current line is blank, you got two newline characters in a row.
@@ -96,11 +115,13 @@ void loop() {
 
             // The HTTP response ends with another blank line:
             client.println();
+            
             // break out of the while loop:
-            break;
+            //break;
           } else {    // if you got a newline, then clear currentLine:
             currentLine = "";
           }
+          
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
         }
@@ -112,13 +133,14 @@ void loop() {
         if (currentLine.endsWith("GET /L")) {
           digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
         }
-        
       }
     }
     // close the connection:
     client.stop();
     Serial.println("Client Disconnected.");
   }
+
+  delay(100);
 }
 
 // Helper Functions
---
+++ sonicSensor/sonicSensor.ino
@@ -0,0 +1,135 @@
+//www.elegoo.com
+//2016.12.08
+
+//// ON/OFF Switch
+//const int buttonPin = 9;    // the number of the pushbutton pin
+//bool ON = false;         // the current state of the output pin (initially OFF)
+//int buttonState;             // the current reading from the input pin
+//int lastButtonState = LOW;   // the previous reading from the input pin
+//unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+//unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+// Ultrasonic Sensor Library
+#include "SR04.h"
+
+// Back Ultrasonic Sensor
+#define BACK_TRIG_PIN 26
+#define BACK_ECHO_PIN 25
+SR04 back_sensor = SR04(BACK_ECHO_PIN, BACK_TRIG_PIN);
+long back_distance;
+
+//// Front Ultrasonic Sensor
+//#define FRONT_TRIG_PIN 3
+//#define FRONT_ECHO_PIN 2
+//SR04 front_sensor = SR04(FRONT_ECHO_PIN, FRONT_TRIG_PIN);
+//long front_distance;
+
+//// DC Motor
+//#define ENABLE 7
+//#define DIRA 6
+//#define DIRB 5
+//float senseResistance = 1.5;
+
+// Controls
+//int RUN_DISTANCE = 10;
+//int BRAKE_DISTANCE = 20;
+
+void setup() {
+
+//  // ON/OFF Switch
+//  pinMode(buttonPin, INPUT);
+  
+//  // Set DC Motor Pin Direction
+//  pinMode(ENABLE,OUTPUT);
+//  pinMode(DIRA,OUTPUT);
+//  pinMode(DIRB,OUTPUT);
+//  pinMode(A0, INPUT);
+  
+  // Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+
+//  // Detect ON/OFF button press
+//  checkForButtonPress();
+//
+//  if (ON) {
+    
+    // Collect & record distance measure
+    back_distance = back_sensor.Distance();
+    Serial.println(back_distance);
+//    front_distance = front_sensor.Distance();
+//    printSensorValues();
+
+//    // Check the motor current
+//    // https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadAnalogVoltage
+//    float sensorValueMax = 1023.0;
+//    float motorVoltageSupply = 3.3;
+//    int sensorValue = analogRead(A0);
+//    float motorVoltage = sensorValue * (motorVoltageSupply / sensorValueMax); // Scaled between 0V and 3.3V
+//    // V = IR -> I = V/R
+//    float motorCurrent = motorVoltage / senseResistance;
+//
+////    Serial.print("sensorValue = ");
+////    Serial.println(sensorValue);
+//    Serial.print("motorVoltage = ");
+//    Serial.println(motorVoltage);
+//    Serial.print("motorCurrent = ");
+//    Serial.println(motorCurrent);
+//
+//    // Turn the motor ON if:
+//    // a threat is close to the backand no frontal obstacles are near.
+//    if ((back_distance < RUN_DISTANCE) && (front_distance > BRAKE_DISTANCE)) {
+//      motorON();
+//    } else {
+//      motorOFF();
+//    }
+//  }
+}
+
+
+// Helper Functions
+
+//void printSensorValues() {
+//    Serial.print("Back = ");
+//    Serial.print(back_distance);
+//    Serial.print("cm");
+//    Serial.print(", ");
+//    Serial.print("Front = ");
+//    Serial.print(front_distance);
+//    Serial.println("cm");
+//    delay(10);
+//}
+//
+//void motorON() {
+////    Serial.println("Motor on");
+//    digitalWrite(ENABLE,255); // enable full speed
+//    digitalWrite(DIRA,HIGH); //one way
+//    digitalWrite(DIRB,LOW);
+//}
+//
+//void motorOFF() {
+//  digitalWrite(ENABLE,LOW);
+//}
+//
+//void checkForButtonPress() {
+//  
+//  int reading = digitalRead(buttonPin);
+//
+//  if (reading != lastButtonState) {
+//    lastDebounceTime = millis();
+//  }
+//  
+//  if ((millis() - lastDebounceTime) > debounceDelay) {
+//    if (reading != buttonState) {
+//      buttonState = reading;
+//      if (buttonState == HIGH) {
+//        ON = !ON; // Switch the ON/OFF state
+//      }
+//    }
+//  }
+//  
+//  lastButtonState = reading;
+//}
